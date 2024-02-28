b579b7ced023545045b029d7bd604e2b396ce06c
HernaldoMeneses <hernaldomeneses@gmail.com>
2024-02-27 21:02:48 -0300

    1.1-Introduction

---
+++ 1.1-Introduction.py
@@ -0,0 +1,10 @@
+''''
+    Scipy is a collection of mathmatical algorithms and convenience function built on the Numpy extension of Python.
+    Providing high-level commands and classes for manipulating and visualizing data.
+'''
+
+##Is all begining here and this way...
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+import matplotlib.pyplot as plt
