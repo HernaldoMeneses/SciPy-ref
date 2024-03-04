6533408b94e52b9b854d4965259a2365a562c89e
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-04 11:25:51 -0300

    Continue 1.1-Introdutino.py

--- 1.1-Introduction.py
+++ 1.1-Introduction.py
@@ -8,3 +8,47 @@ import numpy as np
 import scipy as sp
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+#1.1.1- SciPy Organization
+'''
+    Scipy is organized into subpackages covering diferrent scintific computing domains.
+    following the key summarized
+'''
+
+SciPY_Organization = {
+    'cluster':'Cluestering algorihms',
+    'constants':'Physical and mathematical constants',
+    'fftpack':'Fast Fourier Transform routines',
+    'integrate':'Integration and ordinaty differential equation solvers',
+    'interpolate':'Interpolation and smoothing splines',
+    'io':'Input and Output',
+    'linalg':'Linear algebra',
+    'ndimage':'N-dimensional image processing',
+    'odr':'Orthogonal distance regression',
+    'optimize':'Optimization and root-finding routines',
+    'signal':'Signal processing',
+    'sparse':'Sparse matrices and associeted routines',
+    'spatial':'Spatial data structures and algorithms',
+    'special':'Special functions',
+    'stats':'Statistical distibuitions and functions',
+    'weave':'C/C++ integration',
+}
+
+import pandas as pd
+# Criar DataFrame
+df = pd.DataFrame(list(SciPY_Organization.items()), columns=['Subpackage', 'Description'])
+
+# Exibir DataFrame
+print(df)
+
+#Simple Ex
+from scipy import linalg, optimize
+
+'''
+    Because of their ubiquitousness, some of the functions in these subpackages are also made available in the scipy namespace to ease ther use in interactive sessions and programs.
+    Before looking at the sub-packages individually, we will first look at some of these common functions in basic array functions from numpy
+'''
+
+#1.1.2 Finding Documentation
+#pag.8/1229
+#file:///L:/Linear_/Base-TI/scipy-ref.pdf
---
+++ run.cmd
@@ -0,0 +1,3 @@
+@echo off
+python 1.1-introduction.py 
+cmd
